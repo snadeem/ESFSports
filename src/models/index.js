7b762564394d1565c196ed93d44059beb07dc3cc
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2020-04-06 02:23:54 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -9,9 +9,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
+//const env = process.env.NODE_ENV || 'development';
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config.json')[env];
-//const config = require('/app/build/config/config.json')[env];
+//const config = require(__dirname + '/config.json')[env];
+const config = require('config.json')[env];
 const db = {};
 
 let sequelize;
