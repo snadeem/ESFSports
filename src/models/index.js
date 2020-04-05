5b3517e5ef63cfeba449adb64a8a61a1a9e30518
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2020-04-05 23:58:04 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-import fs  from 'fs';
+/*import fs  from 'fs';
 import path  from 'path';
-import Sequelize  from 'sequelize';
+import Sequelize  from 'sequelize';*/
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
